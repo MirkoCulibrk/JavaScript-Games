c8ebdee9885114abcc9b51e8e9d467e6ccdd2add
MirkoCulibrk <micoculibrk@gmail.com>
2021-01-09 19:33:31 +0100

    finishing snake game

--- Snake canvas/js/script.js
+++ Snake canvas/js/script.js
@@ -1,5 +1,7 @@
 const snakeboard=document.querySelector('#gameCanvas');
 const snakeboard_ctx=snakeboard.getContext('2d');
+const scoreDisplay=document.querySelector('.score');
+const btnStart=document.querySelector('.start');
 //default position for drawing snake
 let snake=[{x:200,y:200},{x:190,y:200},{x:180,y:200},{x:170,y:200}];
 //colors of the snake and board
@@ -8,11 +10,13 @@ const snakeStroke='darkblue';
 const boardBackground='white';
 const boardStroke='black';
 //horizontal movment
-const dx=10;
+let dx=10;
 //vertical movemnet
-const dy=0;
-init();
-
+let dy=0;
+let score=0;
+let foodX;
+let foodY;
+btnStart.addEventListener('click',init,grenerateFood);
 //function for drawing snake
 function drawSnakeParts(snakeParts){
     snakeboard_ctx.fillStyle=snakeFill;
@@ -22,7 +26,6 @@ function drawSnakeParts(snakeParts){
 }
 //function for clearing canvas before game start
 function clearCanvas(){
-    console.log("clear");
     snakeboard_ctx.fillStyle=boardBackground;
     snakeboard_ctx.strokeStyle=boardStroke;
     snakeboard_ctx.fillRect(0,0,snakeboard.width,snakeboard.height);
@@ -34,8 +37,16 @@ function snakeMovement(){
     const snakeHead={x:snake[0].x+dx,y:snake[0].y+dy};
     //adding squeare/head to start of array
     snake.unshift(snakeHead);
-    //removing last element form array
-    snake.pop();
+    const has_eaten=snake[0].x==foodX && snake[0].y==foodY;
+    //if snake head touched food
+    if(has_eaten){
+        score+=1;
+        scoreDisplay.textContent=score;
+        grenerateFood();
+    }else{
+      //removing last element form array
+        snake.pop();  
+    }
 }
 //drawing all parts of snake
 function drawSnake(){
@@ -47,35 +58,83 @@ function drawSnake(){
 function changeDirection(e){
     const UP_ARROW=38;
     const DOWN_ARROW=40;
-    const LEFT_ARROW=40;
-    const RIGHT_ARROW=40;
+    const LEFT_ARROW=37;
+    const RIGHT_ARROW=39;
     const keyPressed=e.keyCode;
-    const goingRight=dx;;
-    const goingUp=-dy;
-    const goingDown=dy;
-    const goingLeft=-dy;
-    if(keyPressed===UP_ARROW && !goingDown){
-        dy=-10;
-        dx=0;
-    }else if(keyPressed===RIGHT_ARROW && !goingLeft){
-        dx=10;
-        dy=0;
-    }else if(keyPressed===LEFT_ARROW && !goingRight){
-        dx=-10;
-        dy=0;
-    }else if(keyPressed===DOWN_ARROW && !goingUp){
-        dx=0;
-        dy=10;
+    const goingRight=dx===10;
+    const goingUp=dy===-10;
+    const goingDown=dy===10;
+    const goingLeft=dx===-10;
+    console.log(dx);
+    if (keyPressed === LEFT_ARROW && !goingRight) {
+        dx = -10;
+        dy = 0;
+      }
+      if (keyPressed === UP_ARROW && !goingDown) {
+        dx = 0;
+        dy = -10;
+      }
+      if (keyPressed === RIGHT_ARROW && !goingLeft) {
+        dx = 10;
+        dy = 0;
+      }
+      if (keyPressed === DOWN_ARROW && !goingUp) {
+        dx = 0;
+        dy = 10;
+      }
+}
+//function for checking if snake hitted wall or tail
+function gameEnded(){
+    //if snake head hitted tail
+    for(let i=4;i<snake.length;i++){
+        const has_colited=snake[i].x===snake[0] && snake[i].y===snake[0].y;
+        if(has_colited){
+            return false;
+        }
     }
+    //returning which wall is hitted
+    const hittedTopWall=snake[0].y<10;
+    const hittedRightWall=snake[0].x>snakeboard.width-20;
+    const hittedBottomWall=snake[0].y>snakeboard.height-20;
+    const hittedLeftWall=snake[0].x<10;
+    return hittedLeftWall||hittedRightWall|| hittedBottomWall|| hittedTopWall;
+}
+//Getting random number for cordination
+function randomFood(min,max){
+    return Math.floor(Math.random()*((max-min)+min)/10)*10;
+}
+foodX=randomFood(0,snakeboard.width-10);
+    foodY=randomFood(0,snakeboard.height-10);
+function grenerateFood(){
+    //-snakeboardw
+    foodX=randomFood(0,snakeboard.width-10);
+    foodY=randomFood(0,snakeboard.height-10);
+    //if food is on snake than draw someore else
+    snake.forEach(function has_eaten_food(part){
+        const has_eaten=part.x==foodX && part.y==foodY;
+        if(has_eaten) grenerateFood();
+    });
+}
+//draw food function
+function drawFood(){
+    snakeboard_ctx.fillStyle = 'lightgreen';
+    snakeboard_ctx.strokestyle = 'darkgreen';
+    snakeboard_ctx.fillRect(foodX,foodY,10,10);
+    snakeboard_ctx.strokeRect(foodX,foodY,10,10)
 }
 document.addEventListener('keyup',changeDirection);
 function init(){
+    if(gameEnded())return;
+
     setTimeout(function onTick(){
+        
         clearCanvas();
+        drawFood();
         snakeMovement();
+        
         drawSnake();
         //caling init to decrease number of calling functions
         init();
-    },500);
+    },200);
     
 }
